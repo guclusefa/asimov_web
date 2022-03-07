a2e5bc39b5bdb641badb45a34335c8d64985adf7
Sefa GUCLU <sefaaguclu@gmail.com>
2022-03-07 02:24:44 +0100

    modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,80 +55,126 @@ $(function () {
 });
 
 /* select2 */
-$( '.select2' ).select2( {
+$('.select2').select2({
   theme: 'bootstrap-5'
 });
 
 $(function () {
   var fr = {
-      searchPanes: {
-          title: {
-              _: 'Filtres séléctionnés - %d',
-              0: 'Aucun filtre séléctionné',
-              1: 'Filtre séléctionné - %d'
-          }
+    searchPanes: {
+      title: {
+        _: 'Filtres séléctionnés - %d',
+        0: 'Aucun filtre séléctionné',
+        1: 'Filtre séléctionné - %d'
+      }
+    },
+    "sProcessing": "Traitement en cours...",
+    "sSearch": "Rechercher&nbsp;: ",
+    "sLengthMenu": "Afficher _MENU_ &eacute;l&eacute;ments",
+    "sInfo": "Affichage de l'&eacute;l&eacute;ment _START_ &agrave; _END_ sur _TOTAL_ &eacute;l&eacute;ments",
+    "sInfoEmpty": "Affichage de l'&eacute;l&eacute;ment 0 &agrave; 0 sur 0 &eacute;l&eacute;ment",
+    "sInfoFiltered": "(filtr&eacute; de _MAX_ &eacute;l&eacute;ments au total)",
+    "sInfoPostFix": "",
+    "sLoadingRecords": "Chargement en cours...",
+    "sZeroRecords": "Aucun &eacute;l&eacute;ment &agrave; afficher",
+    "sEmptyTable": "Aucune donn&eacute;e disponible dans le tableau",
+    "oPaginate": {
+      "sFirst": "Premier",
+      "sPrevious": "Pr&eacute;c&eacute;dent",
+      "sNext": "Suivant",
+      "sLast": "Dernier"
+    },
+    "oAria": {
+      "sSortAscending": ": activer pour trier la colonne par ordre croissant",
+      "sSortDescending": ": activer pour trier la colonne par ordre d&eacute;croissant"
+    },
+    "select": {
+      "rows": {
+        "_": "%d lignes sélectionnées",
+        "0": "Aucune ligne sélectionnée",
+        "1": "1 ligne sélectionnée"
+      }
+    },
+    "searchPanes": {
+      "clearMessage": "Tout effacer",
+      "collapse": {
+        "0": "SearchPanes",
+        "_": "SearchPanes (%d)"
       },
-      "sProcessing": "Traitement en cours...",
-      "sSearch": "Rechercher&nbsp;: ",
-      "sLengthMenu": "Afficher _MENU_ &eacute;l&eacute;ments",
-      "sInfo": "Affichage de l'&eacute;l&eacute;ment _START_ &agrave; _END_ sur _TOTAL_ &eacute;l&eacute;ments",
-      "sInfoEmpty": "Affichage de l'&eacute;l&eacute;ment 0 &agrave; 0 sur 0 &eacute;l&eacute;ment",
-      "sInfoFiltered": "(filtr&eacute; de _MAX_ &eacute;l&eacute;ments au total)",
-      "sInfoPostFix": "",
-      "sLoadingRecords": "Chargement en cours...",
-      "sZeroRecords": "Aucun &eacute;l&eacute;ment &agrave; afficher",
-      "sEmptyTable": "Aucune donn&eacute;e disponible dans le tableau",
-      "oPaginate": {
-          "sFirst": "Premier",
-          "sPrevious": "Pr&eacute;c&eacute;dent",
-          "sNext": "Suivant",
-          "sLast": "Dernier"
-      },
-      "oAria": {
-          "sSortAscending": ": activer pour trier la colonne par ordre croissant",
-          "sSortDescending": ": activer pour trier la colonne par ordre d&eacute;croissant"
-      },
-      "select": {
-          "rows": {
-              "_": "%d lignes sélectionnées",
-              "0": "Aucune ligne sélectionnée",
-              "1": "1 ligne sélectionnée"
-          }
-      },
-      "searchPanes": {
-          "clearMessage": "Tout effacer",
-          "collapse": {
-              "0": "SearchPanes",
-              "_": "SearchPanes (%d)"
-          },
-          "count": "{total}",
-          "countFiltered": "{shown} ({total})",
-          "emptyPanes": "Aucun filtre",
-          "loadMessage": "Chargement filtre",
-          "title": "Filres actifs- %d",
-          "showMessage": "Tout montrer",
-          "collapseMessage": "Tout réduire"
-      },
+      "count": "{total}",
+      "countFiltered": "{shown} ({total})",
+      "emptyPanes": "Aucun filtre",
+      "loadMessage": "Chargement filtre",
+      "title": "Filres actifs - %d",
+      "showMessage": "Tout montrer",
+      "collapseMessage": "Tout réduire"
+    },
   }
   var table = $('#dataTable').DataTable({
-      searchPanes: true,
-      responsive: true,
-      pageLength: 25,
-      language: fr,
-      columnDefs: [
-          {
-              searchPanes: {
-                  show: true,
-                  initCollapsed: true
-              },
-              targets: [0, 1, 2, 3, 4, 5]
-          }
-      ]
+    searchPanes: true,
+    responsive: true,
+    pageLength: 25,
+    language: fr,
+    columnDefs: [
+      {
+        searchPanes: {
+          show: true,
+          initCollapsed: true
+        },
+        targets: [0, 1, 2, 3, 4, 5]
+      }
+    ]
   });
   table.searchPanes.container().prependTo(table.table().container());
   table.searchPanes.resizePanes();
 });
 
+/* dynamic modal */
+// Learn Template literals: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+// Learn about Modal: https://getbootstrap.com/docs/5.0/components/modal/
+
+var modalWrap = null;
+/**
+ * 
+ * @param {string} title 
+ * @param {string} description content of modal body 
+ * @param {string} yesBtnLabel label of Yes button 
+ * @param {string} noBtnLabel label of No button 
+ * @param {function} callback callback function when click Yes button
+ */
+const showModal = (title, description, yesBtnLabel = 'Yes', noBtnLabel = 'Cancel', callback) => {
+  if (modalWrap !== null) {
+    modalWrap.remove();
+  }
+
+  modalWrap = document.createElement('div');
+  modalWrap.innerHTML = `
+    <div class="modal fade" tabindex="-1">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header bg-light">
+            <h5 class="modal-title">${title}</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <p>${description}</p>
+          </div>
+          <div class="modal-footer bg-light">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">${noBtnLabel}</button>
+            <button type="button" class="btn btn-primary modal-success-btn" data-bs-dismiss="modal">${yesBtnLabel}</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
+
+  modalWrap.querySelector('.modal-success-btn').onclick = callback;
+
+  document.body.append(modalWrap);
+
+  var modal = new bootstrap.Modal(modalWrap.querySelector('.modal'));
+  modal.show();
+}
 
 
 
