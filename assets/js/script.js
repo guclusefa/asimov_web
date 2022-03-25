f63dc32830f3dbc5a87241599f6471d211acc39e
GUCLU Sefa <sefaaguclu@gmail.com>
2022-03-25 17:01:23 +0100

    chart js implementation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,149 +1,149 @@
 /* formulaires */
 (function () {
-    'use strict'
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll('.needs-validation')
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms)
-        .forEach(function (form) {
-            form.addEventListener('submit', function (event) {
-                if (!form.checkValidity()) {
-                    event.preventDefault()
-                    event.stopPropagation()
-                }
+  'use strict'
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
 
-                form.classList.add('was-validated')
-            }, false)
-        })
+        form.classList.add('was-validated')
+      }, false)
+    })
 })()
 
 /* dateîcker */
 $(function () {
-    //titre label
-    const getDatePickerTitle = elem => {
-        // From the label or the aria-label
-        const label = elem.nextElementSibling;
-        let titleText = '';
-        if (label && label.tagName === 'LABEL') {
-            titleText = label.textContent;
-        } else {
-            titleText = elem.getAttribute('aria-label') || '';
-        }
-        return titleText;
+  //titre label
+  const getDatePickerTitle = elem => {
+    // From the label or the aria-label
+    const label = elem.nextElementSibling;
+    let titleText = '';
+    if (label && label.tagName === 'LABEL') {
+      titleText = label.textContent;
+    } else {
+      titleText = elem.getAttribute('aria-label') || '';
     }
+    return titleText;
+  }
 
-    const elems = document.querySelectorAll('.datepicker_input');
-    for (const elem of elems) {
-        const datepicker = new Datepicker(elem, {
-            language: "fr",
-            title: getDatePickerTitle(elem),
-            autohide: true,
+  const elems = document.querySelectorAll('.datepicker_input');
+  for (const elem of elems) {
+    const datepicker = new Datepicker(elem, {
+      language: "fr",
+      title: getDatePickerTitle(elem),
+      autohide: true,
 
-        });
-    };
+    });
+  };
 
-    const elems2 = document.querySelectorAll('.datepicker_input_year');
-    for (const elem of elems2) {
-        const datepicker = new Datepicker(elem, {
-            language: "fr",
-            title: getDatePickerTitle(elem),
-            autohide: true,
-            format: 'yyyy',
-            startView: 2,
-            pickLevel: 2,
-            orientation: 'bottom'
-        });
-    };
+  const elems2 = document.querySelectorAll('.datepicker_input_year');
+  for (const elem of elems2) {
+    const datepicker = new Datepicker(elem, {
+      language: "fr",
+      title: getDatePickerTitle(elem),
+      autohide: true,
+      format: 'yyyy',
+      startView: 2,
+      pickLevel: 2,
+      orientation: 'bottom'
+    });
+  };
 });
 
 /* select2 */
 $('.select2').select2({
-    theme: 'bootstrap-5'
+  theme: 'bootstrap-5'
 });
 
 $(function () {
-    var fr = {
-        searchPanes: {
-            title: {
-                _: 'Filtres séléctionnés - %d',
-                0: 'Aucun filtre séléctionné',
-                1: 'Filtre séléctionné - %d'
-            }
-        },
-        "sProcessing": "Traitement en cours...",
-        "sSearch": "Rechercher&nbsp;: ",
-        "sLengthMenu": "Afficher _MENU_ &eacute;l&eacute;ments",
-        "sInfo": "Affichage de l'&eacute;l&eacute;ment _START_ &agrave; _END_ sur _TOTAL_ &eacute;l&eacute;ments",
-        "sInfoEmpty": "Affichage de l'&eacute;l&eacute;ment 0 &agrave; 0 sur 0 &eacute;l&eacute;ment",
-        "sInfoFiltered": "(filtr&eacute; de _MAX_ &eacute;l&eacute;ments au total)",
-        "sInfoPostFix": "",
-        "sLoadingRecords": "Chargement en cours...",
-        "sZeroRecords": "Aucun &eacute;l&eacute;ment &agrave; afficher",
-        "sEmptyTable": "Aucune donn&eacute;e disponible dans le tableau",
-        "oPaginate": {
-            "sFirst": "Premier",
-            "sPrevious": "Pr&eacute;c&eacute;dent",
-            "sNext": "Suivant",
-            "sLast": "Dernier"
-        },
-        "oAria": {
-            "sSortAscending": ": activer pour trier la colonne par ordre croissant",
-            "sSortDescending": ": activer pour trier la colonne par ordre d&eacute;croissant"
-        },
-        "select": {
-            "rows": {
-                "_": "%d lignes sélectionnées",
-                "0": "Aucune ligne sélectionnée",
-                "1": "1 ligne sélectionnée"
-            }
-        },
-        "searchPanes": {
-            "clearMessage": "Tout effacer",
-            "collapse": {
-                "0": "SearchPanes",
-                "_": "SearchPanes (%d)"
-            },
-            "count": "{total}",
-            "countFiltered": "{shown} ({total})",
-            "emptyPanes": "",
-            "loadMessage": "Chargement filtre",
-            "title": "Filres actifs - %d",
-            "showMessage": "Tout montrer",
-            "collapseMessage": "Tout réduire"
-        },
-    }
-    var table = $('#dataTable').DataTable({
-        searchPanes: true,
-        responsive: true,
-        pageLength: 25,
-        language: fr,
-        columnDefs: [{
-            searchPanes: {
-                show: true,
-                initCollapsed: true
-            },
-            /* targets: [0, 1, 2, 3] */
-        }]
-    });
-    table.searchPanes.container().prependTo(table.table().container());
-    table.searchPanes.resizePanes();
+  var fr = {
+    searchPanes: {
+      title: {
+        _: 'Filtres séléctionnés - %d',
+        0: 'Aucun filtre séléctionné',
+        1: 'Filtre séléctionné - %d'
+      }
+    },
+    "sProcessing": "Traitement en cours...",
+    "sSearch": "Rechercher&nbsp;: ",
+    "sLengthMenu": "Afficher _MENU_ &eacute;l&eacute;ments",
+    "sInfo": "Affichage de l'&eacute;l&eacute;ment _START_ &agrave; _END_ sur _TOTAL_ &eacute;l&eacute;ments",
+    "sInfoEmpty": "Affichage de l'&eacute;l&eacute;ment 0 &agrave; 0 sur 0 &eacute;l&eacute;ment",
+    "sInfoFiltered": "(filtr&eacute; de _MAX_ &eacute;l&eacute;ments au total)",
+    "sInfoPostFix": "",
+    "sLoadingRecords": "Chargement en cours...",
+    "sZeroRecords": "Aucun &eacute;l&eacute;ment &agrave; afficher",
+    "sEmptyTable": "Aucune donn&eacute;e disponible dans le tableau",
+    "oPaginate": {
+      "sFirst": "Premier",
+      "sPrevious": "Pr&eacute;c&eacute;dent",
+      "sNext": "Suivant",
+      "sLast": "Dernier"
+    },
+    "oAria": {
+      "sSortAscending": ": activer pour trier la colonne par ordre croissant",
+      "sSortDescending": ": activer pour trier la colonne par ordre d&eacute;croissant"
+    },
+    "select": {
+      "rows": {
+        "_": "%d lignes sélectionnées",
+        "0": "Aucune ligne sélectionnée",
+        "1": "1 ligne sélectionnée"
+      }
+    },
+    "searchPanes": {
+      "clearMessage": "Tout effacer",
+      "collapse": {
+        "0": "SearchPanes",
+        "_": "SearchPanes (%d)"
+      },
+      "count": "{total}",
+      "countFiltered": "{shown} ({total})",
+      "emptyPanes": "",
+      "loadMessage": "Chargement filtre",
+      "title": "Filres actifs - %d",
+      "showMessage": "Tout montrer",
+      "collapseMessage": "Tout réduire"
+    },
+  }
+  var table = $('#dataTable').DataTable({
+    searchPanes: true,
+    responsive: true,
+    pageLength: 25,
+    language: fr,
+    columnDefs: [{
+      searchPanes: {
+        show: true,
+        initCollapsed: true
+      },
+      /* targets: [0, 1, 2, 3] */
+    }]
+  });
+  table.searchPanes.container().prependTo(table.table().container());
+  table.searchPanes.resizePanes();
 });
 
 /* dynamic modal */
 
 var modalWrap = null;
 const showModalDetail = (title, description, lien) => {
-    if (modalWrap !== null) {
-        modalWrap.remove();
-    }
+  if (modalWrap !== null) {
+    modalWrap.remove();
+  }
 
-    let desc = ""
-    Object.keys(description).forEach(function (key) {
-        desc += `<p><b>${key} :</b> ${description[key]}`
-    });
+  let desc = ""
+  Object.keys(description).forEach(function (key) {
+    desc += `<p><b>${key} :</b> ${description[key]}`
+  });
 
-    modalWrap = document.createElement('div');
-    modalWrap.innerHTML = `
+  modalWrap = document.createElement('div');
+  modalWrap.innerHTML = `
     <div class="modal fade" tabindex="-1">
       <div class="modal-dialog">
         <div class="modal-content">
@@ -163,18 +163,18 @@ const showModalDetail = (title, description, lien) => {
     </div>
   `;
 
-    document.body.append(modalWrap);
-    var modal = new bootstrap.Modal(modalWrap.querySelector('.modal'));
-    modal.show();
+  document.body.append(modalWrap);
+  var modal = new bootstrap.Modal(modalWrap.querySelector('.modal'));
+  modal.show();
 }
 
 const showModalSupprimer = (title, description, lien) => {
-    if (modalWrap !== null) {
-        modalWrap.remove();
-    }
+  if (modalWrap !== null) {
+    modalWrap.remove();
+  }
 
-    modalWrap = document.createElement('div');
-    modalWrap.innerHTML = `
+  modalWrap = document.createElement('div');
+  modalWrap.innerHTML = `
   <div class="modal fade" tabindex="-1">
     <div class="modal-dialog">
       <div class="modal-content">
@@ -194,18 +194,18 @@ const showModalSupprimer = (title, description, lien) => {
   </div>
 `;
 
-    document.body.append(modalWrap);
-    var modal = new bootstrap.Modal(modalWrap.querySelector('.modal'));
-    modal.show();
+  document.body.append(modalWrap);
+  var modal = new bootstrap.Modal(modalWrap.querySelector('.modal'));
+  modal.show();
 }
 
-const showModalNotes= (title, description) => {
-    if (modalWrap !== null) {
-        modalWrap.remove();
-    }
+const showModalNotes = (title, description) => {
+  if (modalWrap !== null) {
+    modalWrap.remove();
+  }
 
-    modalWrap = document.createElement('div');
-    modalWrap.innerHTML = `
+  modalWrap = document.createElement('div');
+  modalWrap.innerHTML = `
     <div class="modal fade" tabindex="-1">
       <div class="modal-dialog">
         <div class="modal-content">
@@ -230,7 +230,7 @@ const showModalNotes= (title, description) => {
     </div>
   `;
 
-    document.body.append(modalWrap);
-    var modal = new bootstrap.Modal(modalWrap.querySelector('.modal'));
-    modal.show();
+  document.body.append(modalWrap);
+  var modal = new bootstrap.Modal(modalWrap.querySelector('.modal'));
+  modal.show();
 }
