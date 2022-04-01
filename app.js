482c63cd9c221f14457ad2094bd102eebd2eeacb
Sefa GUCLU <sefaaguclu@gmail.com>
2022-04-01 17:22:49 +0200

    CLEAN

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
-// inclure les dépendances et middlewares
+// inclure les dépendances et middlewares ----------------------------------------------------------------------------------------------
 const Routeur = require("./routes/routes");
 const express = require("express");
 const session = require('express-session');
 const flash = require('connect-flash');
 
-// activer les dépendances
+// activer les dépendances ----------------------------------------------------------------------------------------------
 let app = express();
 app.set("view engine", "ejs");
 
+// bodypardser ----------------------------------------------------------------------------------------------
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
+// session ----------------------------------------------------------------------------------------------
 app.use(session({
 	secret:'leCodeSecretFlash',
 	saveUninitialized: false,
 	resave: false
 }));
 
-// les variables qu'on utilse de partout
+// msg flash + les variables qu'on utilse de partout ----------------------------------------------------------------------------------------------
 app.use(flash())
 app.use(function(req, res, next){
 	valid= req.flash('valid'),
@@ -26,16 +28,19 @@ app.use(function(req, res, next){
     next();
 });
 
-//chemins static
+//chemins static----------------------------------------------------------------------------------------------
 app.use(express.static("views"));
-/* assets */
+
+// assets ----------------------------------------------------------------------------------------------
 app.use("/js", express.static(__dirname + "/assets/js"));
 app.use("/css", express.static(__dirname + "/assets/css"));
 app.use("/images", express.static(__dirname + "/assets/images"));
-/* modules */
+
+// modules ----------------------------------------------------------------------------------------------
 app.use("/js", express.static(__dirname + "/node_modules/jquery/dist"));
 app.use("/js", express.static(__dirname + "/node_modules/bootstrap/dist/js"));
 app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css"));
 
+// lancement serveur ----------------------------------------------------------------------------------------------
 app.listen(3000, () => console.log("Le serveur est actif !"));
 app.use("/", (Routeur));
